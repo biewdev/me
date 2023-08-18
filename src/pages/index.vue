5af1b8c05a5e03c065ac39130ff6a248cae581b1
biewdev <bielaug2002@gmail.com>
2023-08-18 18:02:37 -0300

    refact: separate donut into a component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,66 +1,12 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import Donut from '@/components/Donut.vue';
 import Experience from '../components/Experience.vue';
 import Footer from '../components/Footer.vue';
-
-let donutElement = document.getElementsByClassName('donut');
-
-let A = 1,
-  B = 1;
-
-function donut() {
-  let b: string[] = [];
-  let z: number[] = [];
-
-  A += 0.07;
-  B += 0.03;
-
-  let cA = Math.cos(A),
-    sA = Math.sin(A),
-    cB = Math.cos(B),
-    sB = Math.sin(B);
-
-  for (let k = 0; k < 1760; k++) {
-    b[k] = k % 80 == 79 ? '\n' : ' ';
-    z[k] = 0;
-  }
-
-  for (let j = 0; j < 6.28; j += 0.07) {
-    let ct = Math.cos(j),
-      st = Math.sin(j);
-
-    for (let i = 0; i < 6.28; i += 0.02) {
-      let sp = Math.sin(i),
-        cp = Math.cos(i),
-        h = ct + 2,
-        D = 1 / (sp * h * sA + st * cA + 5),
-        t = sp * h * cA - st * sA;
-
-      let x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
-        y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
-        o = x + 80 * y,
-        N = 0 | (8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB));
-
-      if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
-        z[o] = D;
-        b[o] = '.,-~:;=!*#$@'[N > 0 ? N : 0];
-      }
-    }
-  }
-
-  donutElement[0].innerHTML = b.join('');
-}
-
-onMounted(() => {
-  setInterval(donut, 50);
-});
 </script>
 
 <template>
   <div class="container is-md px-3">
-    <div class="donut-area">
-      <pre class="donut"></pre>
-    </div>
+    <Donut />
 
     <h1 class="title">Gabriel Augusto.<span></span></h1>
 
@@ -81,19 +27,6 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 @import '@colors';
-.donut-area {
-  margin-top: 4rem;
-  overflow: hidden;
-  height: auto;
-  width: auto;
-  display: flex;
-  align-items: start;
-  font-size: 8px;
-  user-select: none;
-  @media (max-width: screen-breakpoint('md')) {
-    display: none;
-  }
-}
 
 .title {
   font-size: $font-size-h1;
